00f2934473911da9beacdb9c6d73956e27c61248
Clifton Gockul <clifga@gmail.com>
2023-03-21 12:34:07 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+const findMatching = (array,names) => array.filter((element)=> element.toLowerCase() == names.toLowerCase());
+
+const fuzzyMatch = (array, letter) => array.filter((element) => (element.charAt(0)).toLowerCase() === letter[0].toLowerCase())
+
+const matchName = (person, string) => person.filter((element) => element.name === string)
